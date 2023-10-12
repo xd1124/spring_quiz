e5ad61adad021e755442899d65be8a908a53b98c
xd1124 <sjtand14@gmail.com>
2023-10-12 14:12:32 +0900

    레슨5 잠깐

---
+++ src/main/java/com/quiz/lesson05/Lesson05Controller.java
@@ -0,0 +1,14 @@
+package com.quiz.lesson05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Lesson05Controller {
+	
+//	@GetMapping("/quiz02")
+//	public String quiz02(Model model) {
+//		
+//	}
+}
